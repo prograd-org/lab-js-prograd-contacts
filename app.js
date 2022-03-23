a9c4ced40ea3e05940fdf2ff9b2358ee03ed74d3
BHARGOB CHOUDHURY <66860621+Bhargob02@users.noreply.github.com>
2022-03-23 08:53:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,64 @@
 //Enter your code here..
-const btn = document.querySelector('#btnGet');
-const msg = document.querySelector('#message');
-btn.onclick = function (){
-    const promise = new Promise((resolve,reject)=>{
-        const request = new XMLHttpRequest;
-        request.open('GET','https://jsonplaceholder.typicode.com/users');
-        request.onload = ()=>{
-            if(request.status === 200){
-                resolve(request.response);
-            }
-            else{
-                reject(Error(request.statusText));
-            }
-        };
-        request.onerror=()=>{
-            reject(Error('Error fetching data'));
-        }
-        request.send();
-    });
-    promise.then((data)=>{
-        console.log('Got data! Promise Executed');
-        const result = JSON.parse(data);
-        var player='<h2>Lists of Users</h2>';
-                    result.forEach(function(user) {
-                     player+=
-                    `<div class="player">
-                      <div class="strength">Name : ${user.name}</div>
-                      <div>Email   : ${user.email}</div>
-                      <div>Phone   : ${user.phone}</div>
-                      <div>Website : ${user.website}</div>
-                      <div>Company : ${user.company.name}</div>
-                      <div>City    : ${user.address.city}</div>
-                      <div>Zipcode : ${user.address.zipcode}</div>
-                     </div>`
-                    });
-        msg.innerHTML=player;
-    },
-    (error)=>{
-        console.log('Promise rejected');
+let users = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then((data) => {
+            return data.json()
+        })
+        .then((data) => {
+            let message = document.querySelector("#message")
+            let h2 = document.createElement("h2")
+            h2.innerHTML = 'Lists of Users'
+            message.append(h2)
+            data.map((ele) => {
+                displaycontact(ele)
+
+            })
+        })
+
+    .catch((error) => {
+        console.log('Promise rejected.');
         console.log(error.message);
-    });
+    })
+}
+document.querySelector("#btnGet").addEventListener("click", users)
+
+function displaycontact(user) {
+    let message = document.querySelector("#message")
+
+    let player = document.createElement("div")
+    player.setAttribute("class", "player")
+
+    let name = document.createElement("div")
+    name.setAttribute("class", "strength")
+    name.innerHTML = 'Name : ' + user.name
+
+    let email = document.createElement("div")
+    email.innerHTML = 'Email : ' + user.email
+
+    let phone = document.createElement("div")
+    phone.innerHTML = 'Phone : ' + user.phone
+
+    let website = document.createElement("div")
+    website.innerHTML = 'Website : ' + user.website
+
+    let company = document.createElement("div")
+    company.innerHTML = 'Company : ' + user.company.name
+
+    let city = document.createElement("div")
+    city.innerHTML = 'City : ' + user.address.city
+
+    let zipcode = document.createElement("div")
+    zipcode.innerHTML = 'Zipcode : ' + user.address.zipcode
+
+
+
+    player.appendChild(name)
+    player.appendChild(email)
+    player.appendChild(phone)
+    player.appendChild(website)
+    player.appendChild(company)
+    player.appendChild(city)
+    player.appendChild(zipcode)
+
+    message.appendChild(player)
 }
