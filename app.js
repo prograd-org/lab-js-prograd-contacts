59d954ea1cbbc5c7cc42ccb7dcbb453381ba2d6b
actuallyroy <amitkumar93525@gmail.com>
2022-07-26 22:53:27 +0530

    19441

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 //Enter your code here..
+document.getElementById("btnGet").onclick = () =>{   
+    fetch("https://jsonplaceholder.typicode.com/users", {method: 'GET'})
+    .then((response)=> response.json())
+    .then((data)=> {
+        console.log(data);
+        data.forEach(element => {
+            document.getElementById("message").innerHTML += `
+            <div class="user-card">
+                <p>
+                    <b class="user-name">Name: ${element.name}</b><br>
+                    Email: ${element.email}<br>
+                    Phone: ${element.phone}<br>
+                    Website: ${element.website}<br>
+                    Company: ${element.company.name}<br>
+                    City: ${element.address.city}<br>
+                    Zipcode: ${element.address.zipcode}<br>
+                </p>
+            </div>`
+        });
+    })
+}
